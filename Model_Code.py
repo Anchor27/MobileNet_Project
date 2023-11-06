ee6dc41aa3e2c687d5d201eb5a39a918e7457ffd
Aryan Joshi <therecommendedguy@gmail.com>
2023-11-07 02:22:20 +0530

    Update Model_Code.py

--- Model_Code.py
+++ Model_Code.py
@@ -15,10 +15,10 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 
 
 ''' LOADING AND PREPROCESSING DATA '''
-X_train = np.loadtxt('/content/drive/MyDrive/Image Classification CNN Keras Dataset/input.csv', delimiter=',')
-Y_train = np.loadtxt('/content/drive/MyDrive/Image Classification CNN Keras Dataset/labels.csv', delimiter=',')
-X_test = np.loadtxt('/content/drive/MyDrive/Image Classification CNN Keras Dataset/input_test.csv', delimiter=',')
-Y_test = np.loadtxt('/content/drive/MyDrive/Image Classification CNN Keras Dataset/labels_test.csv', delimiter=',')
+X_train = np.loadtxt('input.csv', delimiter=',')
+Y_train = np.loadtxt('abels.csv', delimiter=',')
+X_test = np.loadtxt('input_test.csv', delimiter=',')
+Y_test = np.loadtxt('labels_test.csv', delimiter=',')
 
 X_train = X_train.reshape(-1, 100, 100, 3)
 X_test = X_test.reshape(-1, 100, 100, 3)
